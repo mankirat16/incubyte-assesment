893b2247f3a8965ce376eb3a347cb896d46b40ee
Mankirat <mankirat0816@gmail.com>
2025-03-18 10:54:03 +0530

    Feature : Ad implementatioin for supporting multiple delimiters in addition

--- src/string_calculator.ts
+++ src/string_calculator.ts
@@ -1,8 +1,17 @@
 export function add(numbers: string): number {
-    
   if (numbers.trim().length === 0) return 0;
 
-  const all_numbers = numbers.split(",");
+  let delimiter = /,|\n/;
+
+  if (numbers.startsWith("//")) {
+    const parts = numbers.split("\n");
+    delimiter = new RegExp(parts[0].slice(2));
+    numbers = parts[1];
+  }
+
+  const all_numbers = numbers.split(delimiter);
+
+
 
   let sum: number = 0;
 
