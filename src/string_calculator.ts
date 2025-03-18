7a38604179c29650dfe59537c26897b3945ad64f
Mankirat <mankirat0816@gmail.com>
2025-03-18 10:25:19 +0530

    Feature : Add test cases for adding multiple numbers

--- src/string_calculator.ts
+++ src/string_calculator.ts
@@ -1,4 +1,3 @@
 export function add(numbers : string) : number{
     if(numbers.trim().length === 0) return 0;
-    return 5;
 }
--- test/string_calculator.test.ts
+++ test/string_calculator.test.ts
@@ -1,7 +1,16 @@
 import { add } from "../src/string_calculator";
 
-describe('String calculator',()=>{
-    test('should return 0 for empty string',()=>{
-        expect(add("")).toBe(0);
-    })
-})
+describe("String calculator", () => {
+    
+  test("should return 0 for empty string", () => {
+    expect(add("")).toBe(0);
+  });
+
+  test("should return the number itself for a string having only a single number", () => {
+    expect(add("1")).toBe(1);
+  });
+
+  test("should return the sum of numbers separated by a comma", () => {
+    expect(add("1,2,3,4")).toBe(10);
+  });
+});
