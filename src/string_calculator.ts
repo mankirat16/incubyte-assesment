c370cf992e16ef1b3151eaf780feeb2c08789a4b
Mankirat <mankirat0816@gmail.com>
2025-03-18 10:31:33 +0530

    Feature : Implementaion for adding multiple numbers

--- src/string_calculator.ts
+++ src/string_calculator.ts
@@ -1,3 +1,14 @@
-export function add(numbers : string) : number{
-    if(numbers.trim().length === 0) return 0;
+export function add(numbers: string): number {
+    
+  if (numbers.trim().length === 0) return 0;
+
+  const all_numbers = numbers.split(",");
+
+  let sum: number = 0;
+
+  all_numbers.forEach((number) => {
+    sum += Number(number);
+  });
+
+  return sum;
 }
